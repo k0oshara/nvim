ec34cf9a6bb5e7eb01fe29254ab3c88824173c3b
k0oshara <mi9992828@gmail.com>
2026-04-19 23:51:13 +0300

    tabs turn into spaces

--- common.lua
+++ common.lua
@@ -13,6 +13,7 @@ vim.opt.scrolloff = 7
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.winminheight = 0
 vim.opt.winminwidth = 0
