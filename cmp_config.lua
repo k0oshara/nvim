d2505ca8af2783b2375bfadec90b082cb65a436c
k0oshara <mi9992828@gmail.com>
2026-04-21 21:11:31 +0300

    fix autocomplete
    - rename cmp.lua -> cmp_config.lua
    - fix enable/disable autocompplete with ctrl+space
    - add CmpInfo, CmpStatus

--- cmp_config.lua
+++ cmp_config.lua
@@ -5,6 +5,7 @@ local cmp = require'cmp'
 local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
+enabled = true,
 snippet = {
   -- No snippet engine configured yet.
   expand = function(args)
@@ -15,10 +16,17 @@ window = {
   -- completion = cmp.config.window.bordered(),
   -- documentation = cmp.config.window.bordered(),
 },
+preselect = cmp.PreselectMode.Item,
 mapping = cmp.mapping.preset.insert({
   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-Space>'] = cmp.mapping(function()
+    if cmp.visible() then
+      cmp.abort()
+    else
+      cmp.complete()
+    end
+  end, { 'i', 's' }),
   ['<C-e>'] = cmp.mapping.abort(),
   ['<CR>'] = cmp.mapping.confirm({ select = true }),
   ['<Tab>'] = cmp.mapping(function(fallback)
@@ -41,13 +49,26 @@ mapping = cmp.mapping.preset.insert({
 }),
 completion = {
   autocomplete = { cmp.TriggerEvent.TextChanged },
+  keyword_length = 1,
 },
 sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
+  { name = 'nvim_lsp', keyword_length = 1 },
   { name = 'nvim_lsp_signature_help' },
 }, {
   { name = 'buffer' },
-})
+}),
+formatting = {
+  format = function(entry, item)
+    item.menu = ({
+      nvim_lsp = '[LSP]',
+      nvim_lsp_signature_help = '[sig]',
+      buffer = '[buf]',
+      path = '[path]',
+      cmdline = '[cmd]',
+    })[entry.source.name]
+    return item
+  end,
+},
 })
 
 -- Set configuration for specific filetype.
@@ -75,3 +96,16 @@ sources = cmp.config.sources({
   { name = 'cmdline' }
 })
 })
+
+vim.api.nvim_create_user_command('CmpInfo', function()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  local names = vim.tbl_map(function(client)
+    return client.name
+  end, clients)
+
+  vim.notify(table.concat({
+    'cmp visible: ' .. tostring(cmp.visible()),
+    'filetype: ' .. vim.bo.filetype,
+    'lsp clients: ' .. (#names > 0 and table.concat(names, ', ') or '(none)'),
+  }, '\n'), vim.log.levels.INFO, { title = 'Cmp Info' })
+end, { desc = 'Show cmp diagnostics for current buffer' })
