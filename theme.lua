c5501d3ec98d06ff7bade788f2e3eed43e517697
k0oshara <mi9992828@gmail.com>
2026-04-22 01:03:38 +0300

    switch back to gruvbox_hard

--- theme.lua
+++ theme.lua
@@ -1,12 +1,12 @@
 vim.o.background = "dark"
--- vim.g.gruvbox_material_background = "hard"
--- vim.g.gruvbox_material_better_performance = 1
--- vim.g.gruvbox_material_foreground = "material"
--- vim.cmd.colorscheme("gruvbox-material")
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_foreground = "material"
+vim.cmd.colorscheme("gruvbox-material")
 
-require("tokyonight").setup({
-  style = "night",
-  terminal_colors = true,
-})
-
-vim.cmd.colorscheme("tokyonight-night")
+-- require("tokyonight").setup({
+--   style = "night",
+--   terminal_colors = true,
+-- })
+--
+-- vim.cmd.colorscheme("tokyonight-night")
